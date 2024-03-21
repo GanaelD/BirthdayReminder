3ca30d3c0c00fc63b0f2e7355193b4d4b0568286
GanaelD <ganael59320@gmail.com>
2024-03-21 22:51:07 +0100

    - Advanced on django app: configured basic urls - Deleted useless cell in notebook

--- birthday_reminder_app/birthday_reminder_project/asgi.py
+++ birthday_reminder_app/birthday_reminder_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for birthday_reminder_app project.
+ASGI config for birthday_reminder_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birthday_reminder_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birthday_reminder_project.settings')
 
 application = get_asgi_application()
--- birthday_reminder_app/birthday_reminder_project/wsgi.py
+++ birthday_reminder_app/birthday_reminder_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for birthday_reminder_app project.
+WSGI config for birthday_reminder_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birthday_reminder_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birthday_reminder_project.settings')
 
 application = get_wsgi_application()
