4099af16dece32d5e097329d61acf77ead05f3c7
GanaelD <ganael59320@gmail.com>
2024-03-23 10:56:13 +0100

    - Ran migration cmd - Defined birthday model in reminder/models.py

--- birthday_reminder_app/reminder/models.py
+++ birthday_reminder_app/reminder/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Birthday(models.Model):
+    person_name = models.CharField(max_length=20)
+    month = models.IntegerField()
+    day = models.IntegerField()
