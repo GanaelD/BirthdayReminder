526973c756fda753899e61e47ca8de615f4fd882
GanaelD <ganael59320@gmail.com>
2024-03-27 19:14:08 +0100

    - Continued "Build your first django app" tutorial - Added a new jupyter notebook to create birthday reminders from an Excel file

--- birthday_reminder_app/reminder/models.py
+++ birthday_reminder_app/reminder/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 
+#class User(models.Model):
+#    first_name = models.CharField(max_length=20)
+#    last_name = models.CharField(max_length=20)
+#    email = models.CharField(max_length=30)
+
+
 class Birthday(models.Model):
+ #   user = models.ForeignKey(User, on_delete=models.CASCADE)
     person_name = models.CharField(max_length=20)
     month = models.IntegerField()
     day = models.IntegerField()
+
+    def __str__(self):
+        return f"Birthday of {self.person_name}: {self.day}/{self.month}"
